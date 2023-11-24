c7c7e13998d15902ce19d3ff9c90d6de13724661
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-11-24 14:00:23 +0000

    update tests

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-const url = 'https://lybrary-catalog-server-softuni.onrender.com';
+const url = 'http://localhost:3000';
 const userEmail = 'john@abv.bg';
 const secondUserEmail = 'peter@abv.bg';
 
